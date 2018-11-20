b2fe066ba85563f7661bda07bc6398afadd5a896
Felipe Schneeberger <itsshompi@gmail.com>
2018-11-20 02:35:10 -0300

    fix errors

---
+++ precache-manifest.f19faa5a2876bc0a84c4c633bb65b467.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "88d2b154eb0e54338517",
+    "url": "/static/css/main.7c3a9cdc.chunk.css"
+  },
+  {
+    "revision": "939e84ecdb69f0d5ad43226e7894cedc",
+    "url": "/static/media/bg_02.939e84ec.jpg"
+  },
+  {
+    "revision": "8990c6b47c313d0e3f60",
+    "url": "/static/js/1.8990c6b4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "97845cd971881b431cbcb1593e8b549f",
+    "url": "/static/media/me_03.97845cd9.jpg"
+  },
+  {
+    "revision": "88d2b154eb0e54338517",
+    "url": "/static/js/main.88d2b154.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "8990c6b47c313d0e3f60",
+    "url": "/static/css/1.b05be1b3.chunk.css"
+  },
+  {
+    "revision": "182069f21fc0f33fb7b86c51d417967a",
+    "url": "/index.html"
+  }
+];
