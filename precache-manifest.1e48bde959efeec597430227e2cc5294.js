7f504f79d7fe392a8c5a637c41266711e4a5ff87
Felipe Shømpi Schneeberger <itsshompi@gmail.com>
2020-01-29 20:05:38 -0300

    New page 29/01/2020

---
+++ precache-manifest.1e48bde959efeec597430227e2cc5294.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af2b60596ac24539a6eb6854c3f5d23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2bdb12a00f3bf42473d",
+    "url": "/static/css/2.51a2aef8.chunk.css"
+  },
+  {
+    "revision": "8dd76ed6c8cbd4649903",
+    "url": "/static/css/main.52761939.chunk.css"
+  },
+  {
+    "revision": "b2bdb12a00f3bf42473d",
+    "url": "/static/js/2.80598ee7.chunk.js"
+  },
+  {
+    "revision": "327f8fb04d5d680b1f103d4ad0bfcd43",
+    "url": "/static/js/2.80598ee7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8dd76ed6c8cbd4649903",
+    "url": "/static/js/main.be878c8f.chunk.js"
+  },
+  {
+    "revision": "d78bec5ff9f3b5f61e2a",
+    "url": "/static/js/runtime-main.176da99b.js"
+  },
+  {
+    "revision": "ec89e53ca7a95cd1e77b3774747a3b3c",
+    "url": "/static/media/bg_01.ec89e53c.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "d99beea69397632404340411376de3a4",
+    "url": "/static/media/me_04.d99beea6.png"
+  }
+]);
